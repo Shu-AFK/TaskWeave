3cecd4c0e82ad934119ff8905ee2378fb9648383
Floyd <heissewie@gmail.com>
2024-07-23 21:52:06 +0200

    fixed scripts

--- build.bat
+++ build.bat
@@ -1,4 +1,10 @@
-IF NOT EXIST
-    out mkdir out
+IF NOT EXIST out (
+    mkdir out
+)
+
+IF NOT EXIST db (
+    mkdir db
+)
+
 set CGO_ENABLED=1
 go build -o .\out\TaskWeave .\cmd\web\main.go
--- run.bat
+++ run.bat
@@ -1,2 +1,6 @@
+IF NOT EXIST db (
+    mkdir db
+)
+
 set CGO_ENABLED=1
 go run .\cmd\web\main.go
